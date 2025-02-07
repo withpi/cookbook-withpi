b8439dda82d3a53e771ed805383fb9081da6edf1
Zach Clifford <zach@2pir.ai>
2025-02-07 16:02:38 -0500

    Inject common masthead

--- utils/update_masthead.py
+++ utils/update_masthead.py
@@ -6,6 +6,18 @@ run this script from the root of the repository with `python3.11 utils/update_ma
 import json
 from pathlib import Path
 
+def get_colab_link(filename: str) -> dict:
+  return {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        f"<a href=\"https://colab.research.google.com/github/withpi/cookbook-withpi/blob/main/colabs/{filename}.ipynb\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    }
+
 MASTHEAD = {  "cell_type": "markdown",
       "source": [
         "<a href=\"https://withpi.ai\"><img src=\"https://withpi.ai/logoFullBlack.svg\" width=\"240\"></a>\n",
@@ -18,6 +30,50 @@ MASTHEAD = {  "cell_type": "markdown",
         "id": "pi-masthead"
       }
     }
+  
+PI_SETUP_MARKDOWN = {
+      "cell_type": "markdown",
+      "source": [
+        "## Install and initialize SDK\n",
+        "\n",
+        "Connect to a regular CPU Python 3 runtime.  You won't need GPUs for this notebook.\n",
+        "\n",
+        "You'll need a WITHPI_API_KEY from https://play.withpi.ai.  Add it to your notebook secrets (the key symbol) on the left.\n",
+        "\n",
+        "Run the cell below to install packages and load the SDK"
+      ],
+      "metadata": {
+        "id": "pi-setup-markdown"
+      }
+    }
+
+PI_SETUP = {
+      "cell_type": "code",
+      "metadata": {
+        "id": "pi-setup"
+      },
+      "source": [
+        "%%capture\n",
+        "\n",
+        "%pip install withpi litellm\n",
+        "\n",
+        "import os\n",
+        "from google.colab import userdata\n",
+        "from withpi import PiClient\n",
+        "\n",
+        "os.environ[\"WITHPI_API_KEY\"] = userdata.get('WITHPI_API_KEY')\n",
+        "\n",
+        "client = PiClient()"
+      ]
+    }
+
+def write_cell(colab, cell, index):
+  identifier = cell["metadata"]["id"]
+  if colab["cells"][index]["metadata"]["id"] == identifier:
+    colab["cells"][index] = cell
+  else:
+    colab["cells"].insert(index, cell)
+  
 
 def main():
     all_files = list(Path("./colabs").glob("*.ipynb"))
@@ -25,11 +81,12 @@ def main():
         raise ValueError("No Colab notebooks found. Please run this script from the root of the repository.")
     for f in all_files:
         colab = json.loads(f.read_text())
-        if colab["cells"][1]["metadata"].get("id") == "pi-masthead":
-            colab["cells"][1] = MASTHEAD
-        else:
-            colab["cells"].insert(1, MASTHEAD)
-        f.write_text(json.dumps(colab))
+        write_cell(colab, get_colab_link(f.stem), 0)
+        write_cell(colab, MASTHEAD, 1)
+        # 2 should be the introduction cell specific to the notebook
+        write_cell(colab, PI_SETUP_MARKDOWN, 3)
+        write_cell(colab, PI_SETUP, 4)
+        f.write_text(json.dumps(colab, indent=2))
 
 if __name__ == "__main__":
     main()
